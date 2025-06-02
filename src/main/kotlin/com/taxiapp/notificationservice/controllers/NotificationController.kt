e2913dfb848fd766a1d6f9193643c8830209b3ca
Jakub Mikłasz <jmiklasz21@gmail.com>
2025-06-03 01:17:40 +0200

    Fix endpoint path

--- src/main/kotlin/com/taxiapp/notificationservice/controllers/NotificationController.kt
+++ src/main/kotlin/com/taxiapp/notificationservice/controllers/NotificationController.kt
@@ -6,8 +6,11 @@ import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
-@Controller("/api/notification")
+@RestController
+@RequestMapping("/api/notification")
 class NotificationController(
     private val emailService: EmailService
 ) {
