6efb8eba4dcfd22cdde413cee12f486a0546d9c0
Jakub Mikłasz <jmiklasz21@gmail.com>
2025-06-09 11:35:52 +0200

    Add queue listener

--- src/main/kotlin/com/taxiapp/notificationservice/messaging/NotificationMessageReceiver.kt
+++ src/main/kotlin/com/taxiapp/notificationservice/messaging/NotificationMessageReceiver.kt
@@ -1,19 +1,27 @@
 package com.taxiapp.notificationservice.messaging
 
 import com.taxiapp.notificationservice.dto.event.SendEmailRequestEvent
+import com.taxiapp.notificationservice.dto.event.SendPushRequestEvent
 import com.taxiapp.notificationservice.services.EmailService
+import com.taxiapp.notificationservice.services.PushService
 import org.springframework.amqp.rabbit.annotation.RabbitHandler
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.stereotype.Component
 
-@RabbitListener(queues = ["\${rabbit.queue.email.name}"])
+@RabbitListener(queues = ["\${rabbit.queue.email.name}", "\${rabbit.queue.push.name}"])
 @Component
 class NotificationMessageReceiver(
-    private val emailService: EmailService
+    private val emailService: EmailService,
+    private val pushService: PushService
 ) {
     @RabbitHandler
     fun receiveSendEmailRequestEvent(event: SendEmailRequestEvent) {
         emailService.sendEmail(event)
+    }
+
+    @RabbitHandler
+    fun receivePushNotificationEvent(event: SendPushRequestEvent) {
+        pushService.sendPush(event)
     }
 
 }
