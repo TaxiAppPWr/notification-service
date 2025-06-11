32c2664124fd0263292c2774ea268971d713548f
michalK00 <mklemens48@gmail.com>
2025-06-11 03:07:22 +0200

    email sending implementation

--- src/main/kotlin/com/taxiapp/notificationservice/services/EmailServiceImpl.kt
+++ src/main/kotlin/com/taxiapp/notificationservice/services/EmailServiceImpl.kt
@@ -5,13 +5,21 @@ import com.taxiapp.notificationservice.dto.http.EmailSendRequestTO
 import com.taxiapp.notificationservice.dto.http.ResultTO
 import org.springframework.amqp.core.Queue
 import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import java.util.*
+import javax.mail.*
+import javax.mail.internet.InternetAddress
+import javax.mail.internet.MimeMessage
+
 
 @Service
 class EmailServiceImpl(
     private val template: RabbitTemplate,
-    private val emailQueue: Queue
+    private val emailQueue: Queue,
+    @Value("\${mailing.appPassword}") private val appPassword: String,
+    @Value("\${mailing.mailSender}") private val mailSender: String
 ) : EmailService {
     override fun pushEmailToQueue(emailRequest: EmailSendRequestTO): ResultTO {
         template.convertAndSend(
@@ -26,7 +34,44 @@ class EmailServiceImpl(
     }
 
     override fun sendEmail(event: SendEmailRequestEvent) {
-        println("Email sending not implemented")
-        // TODO: Implement email sending logic here - e.g. MailTrap
+        val props = Properties()
+        props["mail.smtp.host"] = "smtp.gmail.com"
+        props["mail.smtp.port"] = "587"
+        props["mail.smtp.auth"] = "true"
+        props["mail.smtp.starttls.enable"] = "true"
+
+        val auth: Authenticator = object : Authenticator() {
+            override fun getPasswordAuthentication(): PasswordAuthentication {
+                return PasswordAuthentication(mailSender, appPassword)
+            }
+        }
+        val session: Session = Session.getInstance(props, auth)
+
+        performSendEmail(session, event.recipientEmail, event.subject, event.body)
+    }
+
+    private fun performSendEmail(session: Session?, toEmail: String?, subject: String?, body: String?) {
+        try {
+            val msg = MimeMessage(session)
+            msg.addHeader("Content-type", "text/HTML; charset=UTF-8")
+            msg.addHeader("format", "flowed")
+            msg.addHeader("Content-Transfer-Encoding", "8bit")
+
+            msg.setFrom(InternetAddress("no_reply@example.com", "NoReply-JD"))
+
+            msg.replyTo = InternetAddress.parse("no_reply@example.com", false)
+
+            msg.setSubject(subject, "UTF-8")
+
+            msg.setText(body, "UTF-8")
+
+            msg.sentDate = Date()
+
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false))
+            Transport.send(msg)
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
