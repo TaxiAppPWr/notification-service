f82275c56e3a7493c1793303e6d9e135b22e5d48
Jakub Mikłasz <jmiklasz21@gmail.com>
2025-06-11 03:10:43 +0200

    Add AWS region

--- src/main/kotlin/com/taxiapp/notificationservice/config/AwsConfig.kt
+++ src/main/kotlin/com/taxiapp/notificationservice/config/AwsConfig.kt
@@ -1,13 +1,18 @@
 package com.taxiapp.notificationservice.config
 
 import aws.sdk.kotlin.services.sns.SnsClient
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class AwsConfig {
+class AwsConfig(
+    @Value("\${aws.region:us-east-1}") private val awsRegion: String = "us-east-1"
+) {
     @Bean
     fun snsClient(): SnsClient {
-        return SnsClient.builder().build()
+        return SnsClient {
+            region = awsRegion
+        }
     }
 }
